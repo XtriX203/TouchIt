1dc1b751dd06dd9690c492278cc40097e369b974
XtriX <com.tomer12@gmail.com>
2021-11-16 17:26:01 +0200

    added support to loading image from hard disc

--- TouchIt/TouchIt/Source.cpp
+++ TouchIt/TouchIt/Source.cpp
@@ -23,6 +23,58 @@ cv::Mat bin;
 
 int main()
 {
+
+
+	std::cout << "choose run option:\n1)live capture\n2)load images\n";
+	int choise;
+	std::cin >> choise;
+
+	switch (choise)
+	{
+	case 1:
+		liveCapture();
+		break;
+	case 2:
+		
+		loadImage();
+		break;
+	default:
+		break;
+	}
+	
+	return 0;
+}
+
+void loadImage()
+{
+	std::string bg;
+	std::string img;
+	std::cout << "Enter path of background:\n";
+	std::cin >> bg;
+
+	std::cout << "Enter path of current image:\n";
+	std::cin >> img;
+
+	cv::Mat background = cv::imread(bg);
+	cv::Mat frame = cv::imread(img);
+
+	// Check for failure
+	if (background.empty() || frame.empty())
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		std::cin.get(); //wait for any key press
+		return;
+	}
+
+
+
+	cv::imshow("original", frame);	//show the RGB frame
+	//cv::imshow("binarization", Background_estimation::MaskBin(background, frame, NULL, frame.rows, frame.cols));
+	// need to decide how to handle with the hand color
+}
+
+void liveCapture()
+{
 	cv::Vec3b handColor = ColorModule::colorManager();//getting the hand color(from the rectangle)
 
 	cv::VideoCapture cap(0);
@@ -31,15 +83,10 @@ int main()
 	cv::Mat background;
 	int mul = 1;
 
-	
-
 	cap.read(frame);
 	flip(frame, frame, 1);
 	avg = frame;
 	bin = frame;
-
-
-	
 
 	std::thread a(checkStop);
 	a.detach();
@@ -68,43 +115,43 @@ int main()
 		cv::imshow("binarization", bin);
 		cv::waitKey(1);
 	}
-	return 0;
 }
-	void Erosion(int, void*)
-	{
-		int erosion_type = 0;
-		if (erosion_elem == 0) {
-			erosion_type = cv::MORPH_RECT;
-		}
-		else if (erosion_elem == 1) {
-			erosion_type = cv::MORPH_CROSS;
-		}
-		else if (erosion_elem == 2) {
-			erosion_type = cv::MORPH_ELLIPSE;
-		}
-		cv::Mat element = cv::getStructuringElement(erosion_type,
-			cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-			cv::Point(erosion_size, erosion_size));
-		cv::erode(bin, bin, element);
-	}
 
-	void Dilation(int, void*)
-	{
-		int dilation_type = 0;
-		if (dilation_elem == 0) {
-			dilation_type = cv::MORPH_RECT;
-		}
-		else if (dilation_elem == 1) {
-			dilation_type = cv::MORPH_CROSS;
-		}
-		else if (dilation_elem == 2) {
-			dilation_type = cv::MORPH_ELLIPSE;
-		}
-		cv::Mat element = cv::getStructuringElement(dilation_type,
-			cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-			cv::Point(dilation_size, dilation_size));
-		dilate(bin, bin, element);
+void Erosion(int, void*)
+{
+	int erosion_type = 0;
+	if (erosion_elem == 0) {
+		erosion_type = cv::MORPH_RECT;
 	}
+	else if (erosion_elem == 1) {
+		erosion_type = cv::MORPH_CROSS;
+	}
+	else if (erosion_elem == 2) {
+		erosion_type = cv::MORPH_ELLIPSE;
+	}
+	cv::Mat element = cv::getStructuringElement(erosion_type,
+		cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		cv::Point(erosion_size, erosion_size));
+	cv::erode(bin, bin, element);
+}
+
+void Dilation(int, void*)
+{
+	int dilation_type = 0;
+	if (dilation_elem == 0) {
+		dilation_type = cv::MORPH_RECT;
+	}
+	else if (dilation_elem == 1) {
+		dilation_type = cv::MORPH_CROSS;
+	}
+	else if (dilation_elem == 2) {
+		dilation_type = cv::MORPH_ELLIPSE;
+	}
+	cv::Mat element = cv::getStructuringElement(dilation_type,
+		cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		cv::Point(dilation_size, dilation_size));
+	dilate(bin, bin, element);
+}
 
 void checkStop()
 {
